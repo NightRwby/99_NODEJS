b6b012ddc49254667f2fa48f687655f2e3aa2a78
NightRwby <jemin9457@naver.com>
2025-10-22 15:21:28 +0900

    v

---
+++ index.js
@@ -0,0 +1,10 @@
+// const _ = require("lodash");
+
+// const arr = [1, 2, 3, 4, 5];
+// const shuffled = _.shuffle(arr);
+// console.log("Shuffled:", shuffled);
+// console.log("HELLOWORLD!!");
+import _ from 'lodash'
+const arr = [1, 2, 3, 4, 5];
+const shuffled = _.shuffle(arr);
+console.log("Shuffled!!!!:", shuffled);
